22ddb18e9605bcf0f9a04ceb7166bc2bffc28fed
galipdogan <galipdogan@gmail.com>
2022-09-01 17:05:02 +0300

    Added and control name exists

---
+++ src/kodlamaIoDevs/Application/Features/ProgrammingLanguages/Profiles/MappingProfiles.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguages;
+using Application.Features.ProgrammingLanguages.Dtos;
+using AutoMapper;
+using Domain.Entities;
+using Core.Persistence.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<ProgrammingLanguage, CreatedProgrammingLanguageDto>().ReverseMap();
+            CreateMap<ProgrammingLanguage, CreateProgrammingLanguageCommand>().ReverseMap();
+            //CreateMap<IPaginate<ProgrammingLanguage>, ProgrammingLanguageListModel>().ReverseMap();
+            // CreateMap<ProgrammingLanguage, ProgrammingLanguageListDto>().ReverseMap();
+            // CreateMap<ProgrammingLanguage, ProgrammingLanguageGetByIdDto>().ReverseMap();
+
+        }
+    }
+}
---
+++ src/kodlamaIoDevs/WebAPI/Controllers/ProgrammingLanguageController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguages;
+using Application.Features.ProgrammingLanguages.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgrammingLanguageController : BaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
+        {
+            CreatedProgrammingLanguageDto result = await Mediator.Send(createProgrammingLanguageCommand);
+            return Created("", result);
+        }
+    }
+}
