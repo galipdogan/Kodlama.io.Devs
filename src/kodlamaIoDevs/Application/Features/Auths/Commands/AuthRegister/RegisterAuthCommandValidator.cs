b935ba8517cfed3fa9458e0b455f8afd32237b8e
galipdogan <galipdogan@outlook.com>
2022-10-06 23:49:51 +0300

    add-secured-operation

---
+++ src/kodlamaIoDevs/Application/Features/Auths/Commands/AuthRegister/RegisterAuthCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Commands.AuthRegister
+{
+    public class RegisterAuthCommandValidator : AbstractValidator<RegisterAuthCommand>
+    {
+        public RegisterAuthCommandValidator()
+        {
+            RuleFor(r => r.UserForRegisterDto.Email).NotEmpty();
+            RuleFor(r => r.UserForRegisterDto.Email).EmailAddress();
+            RuleFor(r => r.UserForRegisterDto.Password).NotEmpty();
+            RuleFor(r => r.UserForRegisterDto.FirstName).NotEmpty();
+            RuleFor(r => r.UserForRegisterDto.LastName).NotEmpty();
+        }
+    }
+}
